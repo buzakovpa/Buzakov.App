4f51a3764d45324a5e546481390b166889eae7b0
Pavel Buzakov <pbuzakov@netlabsystems.com>
2015-03-01 23:24:53 +0500

    + add new Windows Phone Project

---
+++ Buzakov.App.FiveFilms/Buzakov.App.WPFiveFilms/CustomUriMapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Windows.Navigation;
+
+namespace Buzakov.App.WPFiveFilms
+{
+
+    public class CustomUriMapper : UriMapperBase
+    {
+
+        public override Uri MapUri( Uri uri )
+        {
+            var uriToLaunch = HttpUtility.UrlDecode(uri.ToString( ));
+
+            if( uriToLaunch.Contains("wp5films:Show") ) {
+                uri = new Uri("/MainPage.xaml", UriKind.Relative);
+            }
+
+            return uri;
+        }
+
+    }
+
+}
---
+++ Buzakov.App.FiveFilms/Buzakov.App.WPFiveFilms/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Buzakov.App.WPFiveFilms")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Buzakov.App.WPFiveFilms")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5b8892b8-cb3f-4dfb-820a-40a9433fbb79")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
